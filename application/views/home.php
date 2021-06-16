862d843afdd09d2d375208d33e7486ec197fc3db
ardiputra04 <ardidesti04@gmail.com>
2021-06-16 11:01:54 +0700

    membuat menu dan cssnya

--- application/views/home.php
+++ application/views/home.php
@@ -6,10 +6,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $judul; ?></title>
+    <style>
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            background-color: black;
+            padding-top: 10px;
+            padding-left: 10px;
+            padding-bottom: 10px;
+            border-radius: 50px;
+        }
+
+        li {
+            color: white;
+            display: inline-block;
+            margin-left: 15px;
+        }
+    </style>
 </head>
 
 <body>
-
+    <div class="container">
+        <ul>
+            <li href="#">Home</li>
+            <li href="#">Kontak</li>
+            <li href="#">FAQ</li>
+            <li class="li-login" href="#">Login</li>
+        </ul>
+    </div>
 </body>
 
 </html>
